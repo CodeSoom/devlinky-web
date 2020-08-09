9b2d9e176a7ad468542a44407106d911f8731ce7
hannut91 <hannut91@gmail.com>
2020-08-09 16:39:03 +0900

    Remove unused styles
    Remove unused styles.

--- src/App.jsx
+++ src/App.jsx
@@ -2,37 +2,18 @@ import styled from '@emotion/styled';
 
 import React from 'react';
 
-import { colors } from './styles/common/designSystem';
-
 import HomePage from './HomePage';
 
 const Headers = styled.header({
-  display: 'flex',
-  justifyContent: 'space-between',
   padding: '20px 50px',
-  '& span': {
-    textDecoration: 'underline',
-    fontSize: '30px',
-  },
-  '& button': {
-    border: `1.5px solid ${colors.blue.dark}`,
-    borderRadius: '7px',
-    padding: '5px 20px',
-    backgroundColor: colors.blue.dark,
-    color: colors.white,
-    ':hover': {
-      backgroundColor: colors.white,
-      color: colors.blue.dark,
-    },
-  },
+  textDecoration: 'underline',
+  fontSize: '30px',
 });
 
 export default function App() {
   return (
     <>
-      <Headers>
-        <span>#Dev</span>
-      </Headers>
+      <Headers>#Dev</Headers>
       <div>
         <HomePage />
       </div>
