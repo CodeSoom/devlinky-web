e6f0f87d840744b8ed9c087f475c050b0b2fe6be
daadaadaah <60481383+daadaadaah@users.noreply.github.com>
2020-08-11 16:19:32 +0900

    [#41] Manage component by component (#50)
    * Integrate Style code and Component code
    * Remove DevLinks.js in style folder
    * Use Wrapper instead of Container

--- src/DevLinks.jsx
+++ src/DevLinks.jsx
@@ -1,13 +1,10 @@
 import React from 'react';
 
+import styled from '@emotion/styled';
+
 import { ReactTinyLink } from 'react-tiny-link';
 
-import {
-  Container,
-  DevLink,
-  DevLinkBody,
-  LinkContainer,
-} from './styles/DevLinks';
+import { colors } from './styles/common/designSystem';
 
 import DevLinkHeader from './DevLinkHeader';
 import Reviews from './Reviews';
@@ -15,20 +12,75 @@ import Tags from './Tags';
 
 import { isEmpty } from './common/utils';
 
+const Wrapper = styled.div({
+  display: 'flex',
+  justifyContent: 'space-between',
+  flexWrap: 'wrap',
+});
+
+const DevLink = styled.div({
+  flex: '3',
+  margin: '10px',
+  padding: '5px 8px',
+  borderRadius: '5px',
+  backgroundColor: colors.white,
+  textAlign: 'center',
+  boxShadow: `0px 4px 16px 0px ${colors.shadow}`,
+  ': hover': {
+    boxShadow: `0px 8px 32px 2px ${colors.shadow}`,
+    borderRadius: '4px',
+    transition: 'box-shadow 0.25s ease-in 0s, transform 0.25s ease-in 0s',
+  },
+});
+
+const DevLinkBody = styled.div({
+  fontFamily: 'Nanum Pen Script, cursive',
+});
+
+const LinkWrapper = styled.div({
+  backgroundColor: colors.transparent,
+  '& a': {
+    // Link 카드 전체
+    outlineStyle: 'none',
+    height: '100px',
+    width: '28em',
+    margin: '0',
+    padding: '0',
+    '& div': {
+      // Link 타이틀, URL, 도메인이름 등의 Wrapper
+      margin: '2px',
+      padding: '0',
+    },
+    '& header': {
+      // Link 타이틀
+      margin: '0',
+      padding: '0',
+    },
+    '& div div': {
+      // Link URL
+      margin: '0',
+      padding: '0',
+    },
+    '& footer': {
+      // URL 도메인이름
+      margin: '0',
+      padding: '0',
+    },
+  },
+});
+
 export default function DevLinks({ devLinks }) {
   if (isEmpty(devLinks || [])) {
     return <p>로딩중....</p>;
   }
 
   return (
-    <Container>
+    <Wrapper>
       {devLinks.map((devLink) => (
         <DevLink key={devLink.id}>
-          <DevLinkHeader
-            devLink={devLink}
-          />
+          <DevLinkHeader devLink={devLink} />
           <DevLinkBody>
-            <LinkContainer>
+            <LinkWrapper>
               <ReactTinyLink
                 cardSize="medium"
                 showGraphic
@@ -37,16 +89,12 @@ export default function DevLinks({ devLinks }) {
                 minLine={1}
                 url={devLink.url}
               />
-            </LinkContainer>
-            <Tags
-              tags={devLink.tags}
-            />
-            <Reviews
-              reviews={devLink.reviews}
-            />
+            </LinkWrapper>
+            <Tags tags={devLink.tags} />
+            <Reviews reviews={devLink.reviews} />
           </DevLinkBody>
         </DevLink>
       ))}
-    </Container>
+    </Wrapper>
   );
 }
--- src/Tags.jsx
+++ src/Tags.jsx
@@ -1,16 +1,45 @@
 import React from 'react';
 
-import { TagsContainer } from './styles/DevLinks';
+import styled from '@emotion/styled';
+
+import { colors, textStyles } from './styles/common/designSystem';
+
+const { subTitle } = textStyles;
+
+const TagsWrapper = styled.div({
+  display: 'flex',
+  flexDirection: 'row',
+  '& button': {
+    margin: '4px 2px',
+    borderRadius: '5px',
+    border: `1px solid ${colors.orange}`,
+    padding: '2px 10px',
+    color: subTitle.color,
+    fontFamily: subTitle.fontFamily,
+    fontSize: subTitle.fontSize,
+    letterSpacing: subTitle.letterSpacing,
+    backgroundColor: colors.white,
+    ': hover': {
+      backgroundColor: colors.orange,
+      color: colors.white,
+    },
+    ': focus': {
+      outlineStyle: 'none',
+      backgroundColor: colors.orange,
+      color: colors.white,
+    },
+  },
+});
 
 export default function Tags({ tags }) {
   return (
-    <TagsContainer>
+    <TagsWrapper>
       {tags.map((tag) => (
         <button type="button" key={tag.id}>
           #
           {tag.name}
         </button>
       ))}
-    </TagsContainer>
+    </TagsWrapper>
   );
 }
