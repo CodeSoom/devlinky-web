9eb43a4569cebb38b7cb6d4a29da820fb4448c22
daadaadaah <daadaadaah@gmail.com>
2020-08-23 05:51:57 +0900

    Apply devlink's route to app

--- functions/app.js
+++ functions/app.js
@@ -3,11 +3,13 @@ const express = require('express');
 const cors = require('cors');
 
 const hellowordRouter = require('./routes/helloword.route');
+const devlinkRouter = require('./routes/devlink.route');
 
 const app = express();
 app.use(express.json());
 
 app.use(cors({ origin: true }));
 app.use('/hello-devlink-world', hellowordRouter);
+app.use('/devlink', devlinkRouter);
 
 module.exports = app;
