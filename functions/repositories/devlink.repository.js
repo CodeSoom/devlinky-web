c746209b60a64e30c34703de951df15b129c9ce9
daadaadaah <daadaadaah@gmail.com>
2020-08-23 08:51:27 +0900

    Implement repository for reading all devlink

--- functions/repositories/devlink.repository.js
+++ functions/repositories/devlink.repository.js
@@ -7,6 +7,23 @@ const devlinkRepository = {
     resDevLink.id = doc.id;
     return resDevLink;
   },
+  async selectAll() {
+    const devlinks = [];
+
+    const docs = await db.collection('devlink').get();
+    docs.forEach((doc) => {
+      devlinks.push({
+        id: doc.id,
+        url: doc.data().url,
+        writtenAt: doc.data().writtenAt,
+        keyword: doc.data().keyword,
+        tags: doc.data().tags,
+        reviews: doc.data().reviews,
+      })
+    })
+
+    return devlinks;
+  },
 };
 
 module.exports = devlinkRepository;
