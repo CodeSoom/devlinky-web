125072d0f7a7c8f3cf2f89aeb40aec912bc55760
daadaadaah <daadaadaah@gmail.com>
2020-08-23 09:42:50 +0900

    Use dev URL temporarily for fix ci error

--- front-end/services/api.js
+++ front-end/services/api.js
@@ -1,5 +1,5 @@
 export default async function fetchDevLinks() {
-  const url = 'https://us-central1-devlinktag.cloudfunctions.net/app/api/devlinks';
+  const url = 'https://us-central1-devlinktag-dev.cloudfunctions.net/api/devlink/all'; // TODO : 임시로 dev DB 사용
   const response = await fetch(url);
   const data = await response.json();
   return data;
