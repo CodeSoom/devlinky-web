102319ad13e23eca462e323c340540144e29c838
daadaadaah <daadaadaah@gmail.com>
2020-12-24 21:05:31 +0900

    Use new devlink data structure instead of old devlink data structure

--- fixture/data.js
+++ fixture/data.js
@@ -1,381 +1,74 @@
 const devLink = {
-  id: 1,
-  url: 'http://jeonghwan-kim.github.io/2018/07/16/react-app-overview.html',
-  writtenAt: '2020/01',
-  keyword: {
-    id: 1,
-    name: 'Prisma',
-    color: '#0C344B',
-    img: '../assets/images/keyword/prisma.png',
+  id: "uuid1",
+  firstDevlinker: {
+    id: "daadaadaah",
+    img: "https://scontent-ssn1-1.xx.fbcdn.net/v/t31.0-1/cp0/c0.13.80.80a/p80x80/18237765_1282944051818757_9120627240636275285_o.jpg?_nc_cat=105&ccb=2&_nc_sid=7206a8&_nc_ohc=0iQXIG4cESoAX_zTXan&_nc_ht=scontent-ssn1-1.xx&tp=27&oh=f0d27929e45d93d1e4f9aa659111f81f&oe=6009DB1E",
   },
-  tags: [
-    {
-      id: 1,
-      name: '장단점',
-    },
-    {
-      id: 2,
-      name: '역사',
-    },
-    {
-      id: 3,
-      name: '등장배경',
-    },
-  ],
-  reviews: [
-    {
-      id: 1,
-      name: '컴팩트한 글',
-    },
-    {
-      id: 2,
-      name: '초보자도 쉽게 이해할 수 있는 글',
-    },
-  ],
+  url: "https://so4869.tistory.com/23",
+  thumbnamil: "https://i2.wp.com/css-tricks.com/wp-content/uploads/2018/12/guide-flexbox.png?fit=1200%2C600&ssl=1",
+  title: "[HTML, CSS] 요소를 (상하좌우)정렬하기",
+  tags: [{
+    id: 'uuid2',
+    name: 'React',
+    color: '#61dafb',
+  }],
+  comment: "react의 간단 소개",
   createdAt: '2020-02-01 14:10',
-  updatedAt: null,
-  deletedAt: null,
 };
 
 const devLinks = [
   {
-    id: 1,
-    url: 'https://www.prisma.io/',
-    writtenAt: '2020/01',
-    keyword: {
-      id: 1,
-      name: 'Prisma',
-      color: '#0C344B',
-      img: '../assets/images/keyword/prisma.png',
+    id: "uuid1",
+    firstDevlinker: {
+      id: "daadaadaah",
+      img: "https://scontent-ssn1-1.xx.fbcdn.net/v/t31.0-1/cp0/c0.13.80.80a/p80x80/18237765_1282944051818757_9120627240636275285_o.jpg?_nc_cat=105&ccb=2&_nc_sid=7206a8&_nc_ohc=0iQXIG4cESoAX_zTXan&_nc_ht=scontent-ssn1-1.xx&tp=27&oh=f0d27929e45d93d1e4f9aa659111f81f&oe=6009DB1E",
     },
-    tags: [
-      {
-        id: 1,
-        name: '장단점',
-      },
-      {
-        id: 2,
-        name: '역사',
-      },
-      {
-        id: 3,
-        name: '등장배경',
-      },
-    ],
-    reviews: [
-      {
-        id: 1,
-        name: '가독성',
-      },
-      {
-        id: 2,
-        name: '정보성',
-      },
-      {
-        id: 3,
-        name: '통찰력',
-      },
-    ],
-    createdAt: '2020-02-01 14:10',
-    updatedAt: null,
-    deletedAt: null,
-  },
-  {
-    id: 2,
-    url: 'http://jeonghwan-kim.github.io/2018/07/16/react-app-overview.html',
-    writtenAt: '2020/01',
-    keyword: {
-      id: 1,
+    url: "https://so4869.tistory.com/23",
+    thumbnamil: "https://i2.wp.com/css-tricks.com/wp-content/uploads/2018/12/guide-flexbox.png?fit=1200%2C600&ssl=1",
+    title: "[HTML, CSS] 요소를 (상하좌우)정렬하기",
+    tags: [{
+      id: 'uuid2',
       name: 'React',
       color: '#61dafb',
-      img:
-        '../assets/images/keyword/react.png',
-    },
-    tags: [
-      {
-        id: 1,
-        name: '장단점',
-      },
-      {
-        id: 2,
-        name: '역사',
-      },
-      {
-        id: 3,
-        name: '등장배경',
-      },
-    ],
-    reviews: [
-      {
-        id: 1,
-        name: '덕후',
-      },
-      {
-        id: 2,
-        name: '쏙쏙',
-      },
-    ],
-    createdAt: '2020-02-01 14:12',
-    updatedAt: null,
-    deletedAt: null,
-  },
-  {
-    id: 3,
-    url: 'https://graphql.org/',
-    writtenAt: '2020/01',
-    keyword: {
-      id: 1,
-      name: 'GraphQL',
-      color: '#D64292',
-      img: '../assets/images/keyword/graghql.png',
-    },
-
-    tags: [
-      {
-        id: 1,
-        name: '개념',
-      },
-    ],
-    reviews: [
-      {
-        id: 1,
-        name: '스윽',
-      },
-      {
-        id: 2,
-        name: '꾹',
-      },
-    ],
-    createdAt: '2020-02-01 14:14',
-    updatedAt: null,
-    deletedAt: null,
-  },
-  {
-    id: 4,
-    url: 'https://www.prisma.io/',
-    writtenAt: '2020/01',
-    keyword: {
-      id: 1,
-      name: 'Prisma',
-      color: '#0C344B',
-      img: '../assets/images/keyword/prisma.png',
-    },
-    tags: [
-      {
-        id: 1,
-        name: '장단점',
-      },
-      {
-        id: 2,
-        name: '역사',
-      },
-      {
-        id: 3,
-        name: '등장배경',
-      },
-    ],
-    reviews: [
-      {
-        id: 1,
-        name: '가독성',
-      },
-      {
-        id: 2,
-        name: '정보성',
-      },
-      {
-        id: 3,
-        name: '통찰력',
-      },
-    ],
+    }],
+    comment: "react의 간단 소개",
     createdAt: '2020-02-01 14:10',
-    updatedAt: null,
-    deletedAt: null,
   },
   {
-    id: 5,
-    url: 'http://jeonghwan-kim.github.io/2018/07/16/react-app-overview.html',
-    writtenAt: '2020/01',
-    keyword: {
-      id: 1,
+    id: "uuid1",
+    firstDevlinker: {
+      id: "daadaadaah",
+      img: "https://scontent-ssn1-1.xx.fbcdn.net/v/t31.0-1/cp0/c0.13.80.80a/p80x80/18237765_1282944051818757_9120627240636275285_o.jpg?_nc_cat=105&ccb=2&_nc_sid=7206a8&_nc_ohc=0iQXIG4cESoAX_zTXan&_nc_ht=scontent-ssn1-1.xx&tp=27&oh=f0d27929e45d93d1e4f9aa659111f81f&oe=6009DB1E",
+    },
+    url: "https://so4869.tistory.com/23",
+    thumbnamil: "https://i2.wp.com/css-tricks.com/wp-content/uploads/2018/12/guide-flexbox.png?fit=1200%2C600&ssl=1",
+    title: "[HTML, CSS] 요소를 (상하좌우)정렬하기",
+    tags: [{
+      id: 'uuid2',
       name: 'React',
       color: '#61dafb',
-      img:
-        '../assets/images/keyword/react.png',
-    },
-    tags: [
-      {
-        id: 1,
-        name: '장단점',
-      },
-      {
-        id: 2,
-        name: '역사',
-      },
-      {
-        id: 3,
-        name: '등장배경',
-      },
-    ],
-    reviews: [
-      {
-        id: 1,
-        name: '덕후',
-      },
-      {
-        id: 2,
-        name: '쏙쏙',
-      },
-    ],
-    createdAt: '2020-02-01 14:12',
-    updatedAt: null,
-    deletedAt: null,
-  },
-  {
-    id: 6,
-    url: 'https://graphql.org/',
-    writtenAt: '2020/01',
-    keyword: {
-      id: 1,
-      name: 'GraphQL',
-      color: '#D64292',
-      img: '../assets/images/keyword/graghql.png',
-    },
-
-    tags: [
-      {
-        id: 1,
-        name: '개념',
-      },
-    ],
-    reviews: [
-      {
-        id: 1,
-        name: '스윽',
-      },
-      {
-        id: 2,
-        name: '꾹',
-      },
-    ],
-    createdAt: '2020-02-01 14:14',
-    updatedAt: null,
-    deletedAt: null,
-  },
-  {
-    id: 7,
-    url: 'https://www.prisma.io/',
-    writtenAt: '2020/01',
-    keyword: {
-      id: 1,
-      name: 'Prisma',
-      color: '#0C344B',
-      img: '../assets/images/keyword/prisma.png',
-    },
-    tags: [
-      {
-        id: 1,
-        name: '장단점',
-      },
-      {
-        id: 2,
-        name: '역사',
-      },
-      {
-        id: 3,
-        name: '등장배경',
-      },
-    ],
-    reviews: [
-      {
-        id: 1,
-        name: '가독성',
-      },
-      {
-        id: 2,
-        name: '정보성',
-      },
-      {
-        id: 3,
-        name: '통찰력',
-      },
-    ],
+    }],
+    comment: "react의 간단 소개",
     createdAt: '2020-02-01 14:10',
-    updatedAt: null,
-    deletedAt: null,
   },
   {
-    id: 8,
-    url: 'http://jeonghwan-kim.github.io/2018/07/16/react-app-overview.html',
-    writtenAt: '2020/01',
-    keyword: {
-      id: 1,
+    id: "uuid1",
+    firstDevlinker: {
+      id: "daadaadaah",
+      img: "https://scontent-ssn1-1.xx.fbcdn.net/v/t31.0-1/cp0/c0.13.80.80a/p80x80/18237765_1282944051818757_9120627240636275285_o.jpg?_nc_cat=105&ccb=2&_nc_sid=7206a8&_nc_ohc=0iQXIG4cESoAX_zTXan&_nc_ht=scontent-ssn1-1.xx&tp=27&oh=f0d27929e45d93d1e4f9aa659111f81f&oe=6009DB1E",
+    },
+    url: "https://so4869.tistory.com/23",
+    thumbnamil: "https://i2.wp.com/css-tricks.com/wp-content/uploads/2018/12/guide-flexbox.png?fit=1200%2C600&ssl=1",
+    title: "[HTML, CSS] 요소를 (상하좌우)정렬하기",
+    tags: [{
+      id: 'uuid2',
       name: 'React',
       color: '#61dafb',
-      img:
-        '../assets/images/keyword/react.png',
-    },
-    tags: [
-      {
-        id: 1,
-        name: '장단점',
-      },
-      {
-        id: 2,
-        name: '역사',
-      },
-      {
-        id: 3,
-        name: '등장배경',
-      },
-    ],
-    reviews: [
-      {
-        id: 1,
-        name: '덕후',
-      },
-      {
-        id: 2,
-        name: '쏙쏙',
-      },
-    ],
-    createdAt: '2020-02-01 14:12',
-    updatedAt: null,
-    deletedAt: null,
+    }],
+    comment: "react의 간단 소개",
+    createdAt: '2020-02-01 14:10',
   },
-  {
-    id: 9,
-    url: 'https://graphql.org/',
-    writtenAt: '2020/01',
-    keyword: {
-      id: 1,
-      name: 'GraphQL',
-      color: '#D64292',
-      img: '../assets/images/keyword/graghql.png',
-    },
-
-    tags: [
-      {
-        id: 1,
-        name: '개념',
-      },
-    ],
-    reviews: [
-      {
-        id: 1,
-        name: '스윽',
-      },
-      {
-        id: 2,
-        name: '꾹',
-      },
-    ],
-    createdAt: '2020-02-01 14:14',
-    updatedAt: null,
-    deletedAt: null,
-  },
-];
-
+]
 
 module.exports = {
   devLink,
