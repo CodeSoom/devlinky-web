3a71784cbc0b30e2f4849f5da14c62f61c3f17ab
daadaadaah <daadaadaah@gmail.com>
2020-12-27 04:23:35 +0900

    Add onClick test in DevLinkBody compoenent

--- front-end/src/DevLinkBody.test.jsx
+++ front-end/src/DevLinkBody.test.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 
 import DevLinkBody from './DevLinkBody';
 
@@ -13,5 +13,15 @@ describe('<DevLinkBody />', () => {
     devLink.tags.forEach((tag) => expect(container).toHaveTextContent(tag.name));
     expect(getByAltText('링크 썸네일')).toHaveAttribute('src', devLink.thumbnamil);
     expect(container).toHaveTextContent(devLink.title);
+  });
+
+  it('call global.window.open', () => {
+    global.window.open = jest.fn();
+
+    const { getByText } = render(<DevLinkBody devLink={devLink} />);
+
+    fireEvent.click(getByText(devLink.title));
+
+    expect(global.window.open).toBeCalled();
   });
 });
