d96ae29b4d5f858d4dbffb967a2e116ff5aae73b
daadaadaah <daadaadaah@gmail.com>
2020-12-27 04:59:35 +0900

    Update e2e test for home_page for adding new devlink datat structure

--- front-end/test/home_page_test.js
+++ front-end/test/home_page_test.js
@@ -1,53 +1,18 @@
 const devLink = {
-  id: 2,
-  url: 'http://jeonghwan-kim.github.io/2018/07/16/react-app-overview.html',
-  writtenAt: '2020/01',
-  object: {
-    id: 1,
-    name: 'React',
-    color: '#61dafb',
-    img:
-      'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K',
+  id: 'uuid1',
+  firstDevlinker: {
+    id: 'daadaadaah',
+    img: 'https://scontent-ssn1-1.xx.fbcdn.net/v/t31.0-1/cp0/c0.13.80.80a/p80x80/18237765_1282944051818757_9120627240636275285_o.jpg?_nc_cat=105&ccb=2&_nc_sid=7206a8&_nc_ohc=0iQXIG4cESoAX_zTXan&_nc_ht=scontent-ssn1-1.xx&tp=27&oh=f0d27929e45d93d1e4f9aa659111f81f&oe=6009DB1E',
   },
-  tags: [
-    {
-      id: 1,
-      name: '장단점',
-    },
-    {
-      id: 2,
-      name: '역사',
-    },
-    {
-      id: 3,
-      name: '등장배경',
-    },
-    {
-      id: 4,
-      name: '장단점',
-    },
-    {
-      id: 5,
-      name: '역사',
-    },
-    {
-      id: 6,
-      name: '등장배경',
-    },
-  ],
-  reviews: [
-    {
-      id: 1,
-      name: '덕후',
-    },
-    {
-      id: 2,
-      name: '쏙쏙',
-    },
-  ],
-  createdAt: '2020-02-01 14:12',
-  updatedAt: null,
-  deletedAt: null,
+  url: 'https://so4869.tistory.com/23',
+  thumbnamil: 'https://i2.wp.com/css-tricks.com/wp-content/uploads/2018/12/guide-flexbox.png?fit=1200%2C600&ssl=1',
+  title: '[HTML, CSS] 요소를 (상하좌우)정렬하기',
+  tags: [{
+    id: 'uuid2',
+    name: 'React',
+  }],
+  comment: 'react의 간단 소개',
+  createdAt: '2020-02-01 14:10',
 };
 
 Feature('Home');
@@ -61,15 +26,12 @@ Scenario('로고가 보인다.', (I) => {
 Scenario('Link 아이템이 보인다.', (I) => {
   I.amOnPage('/');
 
-  I.see(devLink.object.name);
-
+  I.see(devLink.createdAt);
   devLink.tags.forEach(({ name }) => {
     I.see(name);
   });
 
-  devLink.reviews.forEach(({ name }) => {
-    I.see(name);
-  });
+  I.see(devLink.comment);
 
-  I.see(devLink.writtenAt);
+  I.see(devLink.title);
 });
