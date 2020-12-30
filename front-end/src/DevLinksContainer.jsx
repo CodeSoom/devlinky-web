7f198316fed76fea07b7e2284f31704fe1d0c3e1
daadaadaah <daadaadaah@gmail.com>
2020-12-31 01:43:17 +0900

    Fix lint error

--- front-end/src/DevLinksContainer.jsx
+++ front-end/src/DevLinksContainer.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 
-import { useSelector } from 'react-redux';
-
-import { get, isEmpty } from './common/utils';
+import { isEmpty } from './common/utils';
 
 import DevLinks from './DevLinks';
 
